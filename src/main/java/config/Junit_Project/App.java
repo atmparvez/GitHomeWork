e96225a88b6be2df4ae67f566aef0e3dbee2637b
atmparvez <atmparvez.ny@gmail.com>
2022-02-09 19:58:07 -0600

    Git commit

---
+++ src/main/java/config/Junit_Project/App.java
@@ -0,0 +1,13 @@
+package config.Junit_Project;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
